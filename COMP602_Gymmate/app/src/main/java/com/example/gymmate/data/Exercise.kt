59d015774f73221830cf0901ecc09f10e15a86aa
gnw9753 <gnw9753@autuni.ac.nz>
2023-09-06 02:33:04 +1200

    v0.0.3
    Changes to user structure and added exercise objects

---
+++ COMP602_Gymmate/app/src/main/java/com/example/gymmate/data/Exercise.kt
@@ -0,0 +1,11 @@
+package com.example.gymmate.data
+
+data class Exercise (
+    val exerciseName: String,
+    val difficulty: Int,
+    val pushPull: Int,
+    val modality: Int,
+    val joint: Int,
+    val muscle: String,
+){
+}
--- COMP602_Gymmate/app/src/main/java/com/example/gymmate/data/User.kt
+++ COMP602_Gymmate/app/src/main/java/com/example/gymmate/data/User.kt
@@ -1,4 +1,13 @@
 package com.example.gymmate.data
 
-class User {
+data class User (
+    var name: String,
+    var height: Float,
+    var weight: Float,
+    var age: Int,
+    var difficulty: Int,
+    var daysAvailable: List<String>,
+    var caloriesRequired: Int,
+    ){
+    val bmi: Float = weight / (height*height)
 }
