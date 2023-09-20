652a71139552bad5a221f2662bcd9b959e8e6a38
Deatazd <804782850@qq.com>
2023-09-21 00:08:49 +1200

    good

--- COMP602_Gymmate/app/src/main/java/com/example/gymmate/data/FoodItem.kt
+++
@@ -1,6 +0,0 @@
-package com.example.gymmate.data
-
-class FoodItem {
-    var food: String? = null
-    var size: String? = null
-}
--- COMP602_Gymmate/app/src/main/java/com/example/gymmate/data/UserInfo.kt
+++
@@ -1,8 +0,0 @@
-package com.example.gymmate.data
-
-class UserInfo {
-    var name: String? = null
-    var weight = 0f
-    var height = 0f
-    var goal: String? = null
-}
